2a8a079cc6e120624d157856af785753a570d198
Thien Nguyen <nvtt1510@gmail.com>
2023-11-07 21:51:04 +0700

    product Page and Cart

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,10 +1,33 @@
 <template>
-  <h1>Cart Page</h1>
+  <div id="page-wrap">
+    <h1>Shopping Cart</h1>
+    <div v-for="product in cartItems" :key="product.id" class="product-container">
+      <img class="product-image" :src="product.imageUrl" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>${{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <h3 id="total-price">Total: ${{ totalPrice }}</h3>
+    <button id="checkout-button">Proceed to Checkout</button>
+  </div>
 </template>
 
 <script>
+import { cartItems } from '../fake-data'
 export default {
-  name: 'CartPage'
+  name: 'CartPage',
+  data() {
+    return {
+      cartItems
+    }
+  },
+  computed: {
+    totalPrice() {
+      return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0)
+    }
+  }
 }
 </script>
 
--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Product Page</h1>
+  <div id="page-wrap">
+    <div class="grid-wrap">
+      <div v-for="product in products" class="product-item" v-bind:key="product.id">
+        <img v-bind:src="product.imageUrl" alt="" />
+        <h3 class="product-name">{{ product.name }}</h3>
+        <p class="prodcut-price">${{ product.price }}</p>
+        <router-link v-bind:to="'/products/' + product.id">
+          <button type="button">View Details</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { RouterLink } from 'vue-router'
+import { products } from '../fake-data'
 export default {
-  name: 'ProductsPage'
+  name: 'ProductsPage',
+  data() {
+    return {
+      products
+    }
+  },
+  components: { RouterLink }
 }
 </script>
 
