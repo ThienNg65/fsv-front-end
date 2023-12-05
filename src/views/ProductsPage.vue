99e3562afa5da9fd78d591638cbeb7d4423a208e
Thien Nguyen <nvtt1510@gmail.com>
2023-12-05 09:54:37 +0700

    Grid of the products

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,21 +1,12 @@
 <template>
   <div id="page-wrap">
-    <div class="grid-wrap">
-      <div v-for="product in products" class="product-item" v-bind:key="product.id">
-        <img v-bind:src="product.imageUrl" alt="" />
-        <h3 class="product-name">{{ product.name }}</h3>
-        <p class="prodcut-price">${{ product.price }}</p>
-        <router-link v-bind:to="'/products/' + product.id">
-          <button type="button">View Details</button>
-        </router-link>
-      </div>
-    </div>
+    <ProductsGrid :products="products" />
   </div>
 </template>
 
 <script>
-import { RouterLink } from 'vue-router'
 import { products } from '../fake-data'
+import ProductsGrid from '../components/ProductsGrid.vue'
 export default {
   name: 'ProductsPage',
   data() {
@@ -23,44 +14,6 @@ export default {
       products
     }
   },
-  components: { RouterLink }
+  components: { ProductsGrid }
 }
 </script>
-
-<style scoped>
-.grid-wrap {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 16px;
-}
-
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
-
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
-}
-</style>
