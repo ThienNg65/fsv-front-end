c5f6fcd88faf98fecb3ca0b6b4676d8f09a4bed1
Thien Nguyen <nvtt1510@gmail.com>
2023-12-05 10:41:47 +0700

    Grid for Cart

---
+++ src/components/ProductsList/ProductsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ProductsListItem v-for="product in cartItems" :key="product.id" :product="product" />
+</template>
+
+<script>
+import ProductsListItem from './ProductsListItem.vue'
+export default {
+  name: 'ProductsList',
+  props: ['cartItems'],
+  components: { ProductsListItem }
+}
+</script>
+
+<style scoped>
+.product-container {
+  align-content: 'center';
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  padding: 16px;
+  width: 100%;
+}
+</style>
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="page-wrap">
     <h1>Shopping Cart</h1>
-    <div v-for="product in cartItems" :key="product.id" class="product-container">
-      <img class="product-image" :src="product.imageUrl" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>${{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove from Cart</button>
-    </div>
+    <ProductsList :cartItems="cartItems" />
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
 </template>
 
 <script>
+import ProductsList from '../components/ProductsList/ProductsList.vue'
 import { cartItems } from '../fake-data'
 export default {
   name: 'CartPage',
@@ -27,7 +21,8 @@ export default {
     totalPrice() {
       return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0)
     }
-  }
+  },
+  components: { ProductsList }
 }
 </script>
 
@@ -46,29 +41,5 @@ h1 {
 
 #checkout-button {
   width: 100%;
-}
-
-.product-container {
-  align-content: 'center';
-  border-bottom: 1px solid #ddd;
-  display: flex;
-  padding: 16px;
-  width: 100%;
-}
-
-.product-image {
-  flex: 1;
-  height: 100px;
-  max-width: 100px;
-}
-
-.details-wrap {
-  padding: 0 16px;
-  flex: 3;
-}
-
-.remove-button {
-  flex: 1;
-  margin: auto;
 }
 </style>
