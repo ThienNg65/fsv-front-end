5560c15e7dcf2e2623c941ac7d66f959397700c7
Thien Nguyen <nvtt1510@gmail.com>
2023-12-16 20:30:02 +0700

    Implement add item to cart

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -6,7 +6,13 @@
         <h1>{{ product.name }}</h1>
         <h3 id="price">${{ product.price }}</h3>
         <p>Average rating: {{ product.averageRating }}</p>
-        <button id="add-to-cart">Add to Cart</button>
+        <button v-if="!showSuccessMessage" v-on:click="addToCart" id="add-to-cart">
+          Add to Cart
+        </button>
+        <button v-if="showSuccessMessage" id="add-to-cart" class="green-button">
+          Successfully Added Item to cart!
+        </button>
+
         <h4>Description</h4>
         <p>{{ product.description }}</p>
       </div>
@@ -25,7 +31,20 @@ export default {
   },
   data() {
     return {
-      product: {}
+      product: {},
+      showSuccessMessage: false
+    }
+  },
+
+  methods: {
+    async addToCart() {
+      await axios.post('/api/users/12345/cart', {
+        productId: this.$route.params.id
+      })
+      this.showSuccessMessage = true
+      setTimeout(() => {
+        this.$router.push('/products')
+      }, 1500)
     }
   },
 
